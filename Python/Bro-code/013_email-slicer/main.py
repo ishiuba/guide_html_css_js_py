8eae61024604adad26e150ba2ebb28be459737d7
iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
2025-01-04 11:18:51 -0300

    Add Python examples for conditional expressions, string methods, string indexing, email slicing, and format specifiers

---
+++ Python/Bro-code/013_email-slicer/main.py
@@ -0,0 +1,10 @@
+email = input("Enter your email: ")
+
+#index = email.index("@")
+
+#username = email[:index]
+username = email[:email.index("@")]
+#domain = email[index +1:]
+domain = email[email.index("@") +1:]
+
+print(f"Your username is {username} and domain is {domain}")
