6d758053acc247197016f08b780ad7d8f51d9a63
iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
2025-01-09 11:31:05 -0300

    Add Python examples for iterables, membership operators, list comprehension, match-case statements, and modules

---
+++ Python/Bro-code/035_iterables/main.py
@@ -0,0 +1,44 @@
+# iterables = an object/collection that can return its elements one at a time, allowing it to be iterated over in a loop
+
+numbers = [1, 2, 3, 4, 5]
+
+for number in numbers:
+    print(number)
+
+numbers = (1, 2, 3, 4, 5)
+
+for number in numbers:
+    print(number)
+
+print()
+
+print("-----------------------------------------------------------")
+
+fruits = {"apple", "orange", "banana", "coconut"}
+
+for fruit in fruits:
+    print(fruit)
+
+print("-----------------------------------------------------------")
+
+name = "IamSHIUBA"
+
+for character in name:
+    print(character, end=" ")
+
+print("-----------------------------------------------------------")
+
+my_dictionary = {
+    "A": 1,
+    "B": 2,
+    "C": 3
+}
+
+for key in my_dictionary:
+    print(key)
+
+for value in my_dictionary.values():
+    print(value)
+
+for key, value in my_dictionary.items():
+    print(f"{key} - {value}")
