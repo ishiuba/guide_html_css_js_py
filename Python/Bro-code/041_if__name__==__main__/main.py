01bf7344c183633408f5d21d35b6e2fa50ce0ce8
iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
2025-01-10 12:14:26 -0300

    Add Python scripts for variable scope, module execution, credit card validation, banking program, and slot machine game

---
+++ Python/Bro-code/041_if__name__==__main__/main.py
@@ -0,0 +1,15 @@
+# if __name__ == __main__: (this script can be imported OR run standalone)
+#                           functions and classes in this module can be reused
+#                           without the main block of code executing
+#                           Good practice (Code is modular, helps readability,
+#                           leaves no global variables, avoid unintended execution)
+#                           ex: library = import library for functionality
+#                           When running library directly, display a help page
+
+
+
+def main():
+    # your program goes here
+
+if __name__ == '__main__':
+    main()
---
+++ Python/Bro-code/041_if__name__==__main__/script1.py
@@ -0,0 +1,13 @@
+
+print(__name__)
+
+def favorite_food(food):
+    print(f"Your favorite food is {food}")
+
+def main():
+    print("This is script1")
+    favorite_food('chocolate')
+    print("Goodbye!")
+
+if __name__ == '__main__':
+    main()
