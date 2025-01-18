9ec520cd6326e5ce8828dd9886c5804e15d38586
iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
2025-01-18 09:50:57 -0300

    Remove unused files and examples from Python and JavaScript directories

--- JavaScript/web_dev_simplified/Event-Listener/script.js
+++
@@ -1,7 +0,0 @@
-const grandparent = document.querySelector(".grandparent");
-const parent = document.querySelector(".parent");
-const child = document.querySelector(".child");
-
-grandparent.addEventListener("click", (e) => {
-  console.log(e);
-});
